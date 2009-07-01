5234e6b64ee8b3f1b28062bc81c46a9356c10089
mruiz <mackhitler@gmail.com>
2009-07-01 10:20:42 -0400

    adicionar paginacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,15 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :adicionar_paginacion
   # Para poder mostrar Ajax
   layout proc{|controller| controller.request.xhr? ? false : "application"}
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  protected
+
+  def adicionar_paginacion
+    @page = params[:page] || 1
+  end
 end
